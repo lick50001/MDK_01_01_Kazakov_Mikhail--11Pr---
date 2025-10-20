8c72aa6f4561c2940b2ad51c8cc9056565995da9
qwel0rd <secondofall22@rambler.ru>
2025-10-20 16:39:38 +0500

    Готовая практическая работа

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,14 +15,41 @@ using System.Windows.Shapes;
 
 namespace _11Pr_Kazakov
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private List<string> students = new List<string>
+        {
+            "Казаков Михаил Андреевич",
+            "Петров Евгений Сергеевич",
+            "Иванов Иван Иванович",
+            "Смирнов Владислав Олегович",
+            "Кузнецов Степан Дмитриевич",
+            "Рамазанов Назир Магомедович",
+            "Соколов Максим Алексеевич",
+            "Волков Николай Павлович"
+        };
+
         public MainWindow()
         {
             InitializeComponent();
+            DisplayNames();
+        }
+
+        public void DisplayNames()
+        {
+            DisplayNames(", ");
+        }
+
+        public void DisplayNames(string znak)
+        {
+            string result = string.Join(znak, students);
+            ResultOutput.Content = result;
+        }
+
+        private void ShowInfo(object sender, RoutedEventArgs e)
+        {
+            string znak = SeparatorInput.Text;
+            DisplayNames(znak);
         }
     }
 }
